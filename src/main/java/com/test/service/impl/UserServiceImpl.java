2296efe39e682110ab5f8328e0a53f61cc4ec4eb
hujia <hujia@healthsource.com.cn>
2019-05-22 13:59:17 +0800

    springmvc处理器映射器配置问题

--- src/main/java/com/test/service/impl/UserServiceImpl.java
+++ src/main/java/com/test/service/impl/UserServiceImpl.java
@@ -1,9 +1,16 @@
 package com.test.service.impl;
 
 import com.test.entity.User;
+import com.test.mapper.UserMapper;
 import com.test.service.UserService;
 
 public class UserServiceImpl implements UserService {
+
+	private UserMapper userMapper;
+
+	public void setUserMapper(UserMapper userMapper) {
+		this.userMapper = userMapper;
+	}
 
 	public boolean addUrserInfo(User user) {
 		return false;
--- src/main/java/com/test/web/UserController.java
+++ src/main/java/com/test/web/UserController.java
@@ -1,4 +1,19 @@
 package com.test.web;
 
+import com.test.service.UserService;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
 public class UserController {
+
+	private UserService userService;
+
+	public void setUserService(UserService userService) {
+		this.userService = userService;
+	}
+
+	@RequestMapping("/test")  //就是想替换掉这个@RequestMapping
+	public ModelAndView getUser(){
+		return new ModelAndView();
+	}
 }
